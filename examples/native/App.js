984f6d9584b89c040532888fc021a9de804cecf9
Yuku Kotani <yukukotani@gmail.com>
2024-03-24 16:13:23 +0900

    show webview

--- examples/native/App.js
+++ examples/native/App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
+import WebView from "react-native-webview";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <WebView style={styles} source={{ uri: "http://localhost:5173" }} />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
